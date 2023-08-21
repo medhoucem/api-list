756523154645dacc9afa52eb85789e5570d2d7ae
medhoucem <houcemghrairi@gmail.com>
2023-08-21 16:31:53 +0100

    api-list

---
+++ src/components/UserList.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import UserCard from './UserCard'
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+
+const UserList = () => {
+    const [users, setusers] = useState([])
+    useEffect(() => {
+      (async()=>{
+        try {
+          let result=await axios.get("https://jsonplaceholder.typicode.com/users")
+          setusers(result.data)
+        } catch (error) {
+          console.log(error)
+        }
+      })()
+     
+    }, [])
+  return (
+    <div className='list'>
+     {users.map(el=><UserCard el={el}/>)}
+    </div>
+  )
+}
+
+export default UserList
